37a6a42a6156744ea7ad350e890b52e733ff70cc
chriswaudby <cwaudby@gmail.com>
2023-05-15 16:59:39 +0100

    Separate out JCAMP-DX parsing

--- src/NMRIO/NMRIO.jl
+++ src/NMRIO/NMRIO.jl
@@ -4,8 +4,10 @@ using ..NMRBase
 
 include("loadnmr.jl")
 include("loadnmrpipe.jl")
+include("jdx.jl")
 include("acqus.jl")
 
 export loadnmr
+export loadjdx
 
 end
--- src/NMRIO/acqus.jl
+++ src/NMRIO/acqus.jl
@@ -46,62 +46,12 @@ end
 
 
 function parseacqus(acqusfilename::String)
-    isfile(acqusfilename) || throw(NMRToolsException("loading acqus data: $(acqusfilename) is not a valid file"))
-
-    dat = open(acqusfilename) do f
-        read(f, String)
-    end
-
-    fields = [strip(strip(x),'$') for x in split(dat, "##")]
-    dic = Dict{String,Any}()
-    for field in fields
-        field == "" && continue # skip empty lines
-        x = split(field, "= ")
-        if length(x) > 1
-            # store keys as uppercase - there's only a couple of annoying exceptions like NusAMOUNT
-            # and forcing uppercase makes it easier to access, e.g. as :vclist not :VCLIST
-            dic[uppercase(x[1])] = parseacqusentry(x[2])
-        end
-    end
+    dic = loadjdx(acqusfilename)
 
     # lastly, check for referenced files like vclist, fq1list, and load these in place of filename
     parseacqusauxfiles!(dic, dirname(acqusfilename))
 
     return dic
-end
-
-
-
-function parseacqusentry(dat)
-    if dat[1] == '('
-        # array data - split into fields
-        fields = split(split(dat, ")\n")[2])
-        parsed = map(x -> parseacqusfield(x), fields)
-        if parsed isa Vector{Real}
-            parsed = float.(parsed)
-        end
-        # create a dictionary, 0 => p0, 1 => p1, etc.
-        parsed = Dict(zip(0:length(parsed)-1, parsed))
-    else
-        parsed = parseacqusfield(dat)
-    end
-    return parsed
-end
-
-
-
-function parseacqusfield(dat)
-    if dat[1] == '<' # <string>
-        dat = dat[2:end-1]
-    else
-        x = tryparse(Int64, dat)
-        if isnothing(x)
-            dat = tryparse(Float64, dat)
-        else
-            dat = x
-        end
-    end
-    return dat
 end
 
 
